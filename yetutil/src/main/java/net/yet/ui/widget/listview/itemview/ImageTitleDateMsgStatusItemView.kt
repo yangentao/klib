20596b92510605470de63591283a6a8d3be02af2
yangentao <entaoyang@163.com>
2016-08-27 19:38:01 +0800

    ImageTitleDateMsgStatusItemView

--- yetutil/src/main/java/net/yet/ui/widget/listview/itemview/ImageTitleDateMsgStatusItemView.kt
+++ yetutil/src/main/java/net/yet/ui/widget/listview/itemview/ImageTitleDateMsgStatusItemView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import net.yet.theme.Dim
 import net.yet.ui.ext.*
@@ -22,59 +23,68 @@ import net.yet.util.DateUtil
  * -----------------------------------------------
  */
 class ImageTitleDateMsgStatusItemView(context: Context) : HorItemView(context) {
-    var iconView: ImageView = createImageView()
-    var titleView: TextView = createTextViewB().singleLine().ellipsizeEnd()
-    var dateView: TextView = createTextViewC().singleLine()
-    var msgView: TextView = createTextViewC().singleLine().ellipsizeEnd()
-    var statusView: TextView = createTextViewC().singleLine()
-    private var iconSize = Dim.iconSizeMid
+	var iconView: ImageView = createImageView()
+	var titleView: TextView = createTextViewB().singleLine().ellipsizeEnd()
+	var dateView: TextView = createTextViewC().singleLine()
+	var msgView: TextView = createTextViewC().singleLine().ellipsizeEnd()
+	var statusView: TextView = createTextViewC().singleLine()
+	private var iconSize = Dim.iconSizeMid
 
-    init {
-        padding(10, 0, 10, 0)
-        addViewParam(iconView) { size(iconSize).gravityCenter().margins(0, 0, 5, 0) }
+	private var topView = createLinearHorizontal().gravityCenterVertical()
+	private var bottomView = createLinearHorizontal().gravityCenterVertical()
 
-        val ll = createLinearVertical().apply {
-            val top = createLinearHorizontal().gravityCenterVertical().apply {
-                addViewParam(titleView) { widthDp(0).heightWrap().weight(1f) }
-                addView(dateView, linearParam().wrap().margins(5, 0, 0, 0))
-            }
-            addViewParam(top) { widthFill().heightWrap() }
-            val bottom = createLinearHorizontal().gravityCenterVertical().apply {
-                addViewParam(msgView) { widthDp(0).heightWrap().weight(1f) }
-                addViewParam(statusView) { wrap().margins(5, 0, 0, 0) }
-            }
-            addViewParam(bottom) { widthFill().heightWrap() }
-        }
-        addViewParam(ll) { widthDp(0).weight(1f).heightWrap().margins(5, 10, 5, 5) }
-    }
+	init {
+		padding(10, 0, 10, 0)
+		addViewParam(iconView) { size(iconSize).gravityCenter().margins(0, 0, 5, 0) }
 
-    fun setIconSize(dp: Int): ImageTitleDateMsgStatusItemView {
-        this.iconSize = dp
-        return this
-    }
+		val ll = createLinearVertical().apply {
+			val top = topView.apply {
+				addViewParam(titleView) { widthDp(0).heightWrap().weight(1f) }
+				addView(dateView, linearParam().wrap().margins(5, 0, 0, 0))
+			}
+			addViewParam(top) { widthFill().heightWrap() }
+			val bottom = bottomView.apply {
+				addViewParam(msgView) { widthDp(0).heightWrap().weight(1f) }
+				addViewParam(statusView) { wrap().margins(5, 0, 0, 0) }
+			}
+			addViewParam(bottom) { widthFill().heightWrap() }
+		}
+		addViewParam(ll) { widthDp(0).weight(1f).heightWrap().margins(5, 10, 5, 5) }
+	}
+
+	fun setSepratorSize(dp: Int) {
+		val p = topView.layoutParams as LinearLayout.LayoutParams
+		p.margins(p.leftMargin, p.topMargin, p.rightMargin, dp)
+		topView.layoutParams = p
+	}
+
+	fun setIconSize(dp: Int): ImageTitleDateMsgStatusItemView {
+		this.iconSize = dp
+		return this
+	}
 
 
-    fun setValues(icon: Drawable?, title: String, date: String, msg: String, status: String): ImageTitleDateMsgStatusItemView {
-        iconView.setImageDrawable(icon)
-        titleView.text = title
-        dateView.text = date
-        msgView.text = msg
-        statusView.text = status
-        return this
-    }
+	fun setValues(icon: Drawable?, title: String, date: String, msg: String, status: String): ImageTitleDateMsgStatusItemView {
+		iconView.setImageDrawable(icon)
+		titleView.text = title
+		dateView.text = date
+		msgView.text = msg
+		statusView.text = status
+		return this
+	}
 
-    fun setValues(icon: Drawable?, title: String, date: Long, msg: String, status: String): ImageTitleDateMsgStatusItemView {
-        val s = if (date == 0L) "" else DateUtil.shortString(date)
-        return setValues(icon, title, s, msg, status)
-    }
+	fun setValues(icon: Drawable?, title: String, date: Long, msg: String, status: String): ImageTitleDateMsgStatusItemView {
+		val s = if (date == 0L) "" else DateUtil.shortString(date)
+		return setValues(icon, title, s, msg, status)
+	}
 
-    fun showRedPoint(show: Boolean): ImageTitleDateMsgStatusItemView {
-        statusView.setCompoundDrawables(null, null, if (show) redDrawable else null, null)
-        return this
-    }
+	fun showRedPoint(show: Boolean): ImageTitleDateMsgStatusItemView {
+		statusView.setCompoundDrawables(null, null, if (show) redDrawable else null, null)
+		return this
+	}
 
-    companion object {
-        private val redDrawable = ShapeUtil.oval(10, Color.rgb(255, 128, 0))
-    }
+	companion object {
+		private val redDrawable = ShapeUtil.oval(10, Color.rgb(255, 128, 0))
+	}
 
 }
