693bcb7a17aa5defcb4a252f2cd09cc8011777b1
yangentao <entaoyang@163.com>
2016-11-12 17:31:19 +0800

    image scale

--- yetutil/src/main/java/net/yet/ui/ext/ImaveViewExt.kt
+++ yetutil/src/main/java/net/yet/ui/ext/ImaveViewExt.kt
@@ -6,18 +6,38 @@ import android.widget.ImageView
  * Created by entaoyang@163.com on 16/3/13.
  */
 
+fun <T : ImageView> T.scaleCenter(): T {
+	this.scaleType = ImageView.ScaleType.CENTER
+	return this
+}
 
-inline fun <reified T : ImageView> T.scaleCenterInside(): T {
+fun <T : ImageView> T.scaleCenterInside(): T {
 	this.scaleType = ImageView.ScaleType.CENTER_INSIDE
 	return this
 }
 
-inline fun <reified T : ImageView> T.scaleFitXY(): T {
+fun <T : ImageView> T.scaleCenterCrop(): T {
+	this.scaleType = ImageView.ScaleType.CENTER_CROP
+	return this
+}
+
+
+fun <T : ImageView> T.scaleFitXY(): T {
 	this.scaleType = ImageView.ScaleType.FIT_XY
 	return this
 }
 
-inline fun <reified T : ImageView> T.scaleFitCenter(): T {
+fun <T : ImageView> T.scaleFitCenter(): T {
 	this.scaleType = ImageView.ScaleType.FIT_CENTER
+	return this
+}
+
+fun <T : ImageView> T.scaleFitStart(): T {
+	this.scaleType = ImageView.ScaleType.FIT_START
+	return this
+}
+
+fun <T : ImageView> T.scaleFitEnd(): T {
+	this.scaleType = ImageView.ScaleType.FIT_END
 	return this
 }
